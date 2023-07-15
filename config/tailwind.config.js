17814f47aac9bcc0a8caa50cd1d45c50c33f7d26
jealaine0128 <engr.jamielaine@gmail.com>
2023-07-15 20:33:43 +0800

    Final Commit
    Final Blog App

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
